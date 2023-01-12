3d2941a69e02305ebfb3f1bb28b8d248e08ecd8a
Ed Oliveros <edgarolimoy98@gmail.com>
2023-01-12 11:17:05 -0500

    es11 terminado

---
+++ src/es11/04-globalThis.js
@@ -0,0 +1,6 @@
+// acceder a un objeto global
+console.log(window); //navegador
+console.log(global); // node
+console.log(selft); // webworker
+console.log(globalThis); // estandar para todo
+
--- src/es11/05-matchall.js
+++ src/es11/05-matchall.js
@@ -4,4 +4,4 @@ const fruit = "Apple, Banana, Kiwi, Banana, Orange, etc, etc, etc, ";
 
 for (const match of fruit.matchAll(regex)) {
     console.log(match);
-}
+} 
